4d6875c04c5dd236239fcfa75dcba923ee02a89e
Rob Lourens <roblourens@gmail.com>
2025-08-05 15:43:46 -0700

    Revert test commit

--- src/platform/configuration/common/configurationService.ts
+++ src/platform/configuration/common/configurationService.ts
@@ -153,7 +153,6 @@ export interface IConfigurationService {
 }
 
 
-// test
 export abstract class AbstractConfigurationService extends Disposable implements IConfigurationService {
 	declare readonly _serviceBrand: undefined;
 
